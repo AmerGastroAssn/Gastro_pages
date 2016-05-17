8f4611ee1e7d3c1f8196e23524598199f9ac8fa4
cbstodd <cbstodd@gmail.com>
2016-05-17 11:04:17 -0400

    added NPPA microsite pages

---
+++ pgcourse/twitter_feed.js
@@ -0,0 +1,10 @@
+<a class="twitter-timeline" href="https://twitter.com/hashtag/AGAPG" data-widget-id="730479900050763776" width="300" height="420">#AGAPG Tweets</a>
+<script>!function ( d, s, id ){
+  var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+  if ( !d.getElementById(id) ){
+    js = d.createElement(s);
+    js.id = id;
+    js.src = p + "://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+}(document, "script", "twitter-wjs");</script>
