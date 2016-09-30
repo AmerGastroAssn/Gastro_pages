0fb4c80f0a6c7fc5f57103bce787cfa31f9ab04e
cbstodd <cbstodd@gmail.com>
2016-09-30 13:38:33 -0400

    added agaperspectives.gastro.org css

--- js/index.js
+++ js/index.js
@@ -1,169 +1,51 @@
-<!--<h3>Friday, Feb. 5, 2016</h3>
+// Load required packages
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
-<
-table >
-    <
-    tbody >
-    <
-    tr >
-    <
-    td > < strong > 12: 30 & ndash;
-1 p.m. < /strong></td >
-    <
-    td > < strong > Check - In < /strong></td >
-    <
-    /tr> <
-    tr >
-    <
-    td > < strong > 1 < /strong><strong style="line-height: 20.8px;">&ndash;</strong > < strong > 1: 15 p.m. < /strong></td >
-    <
-    td > Welcome and Introductions / Program Overview < /td> <
-    /tr> <
-    tr >
-    <
-    td > < strong > 1: 15 & ndash;
-1: 45 p.m. < /strong></td >
-    <
-    td > Your Most Unique and / or Challenging Moment: < br / >
-    Ice Breaker Team Challenge < /td> <
-    /tr> <
-    tr >
-    <
-    td style = "width: 25%; vertical-align: top;" > < strong > 1: 45 & ndash;
-2: 45 p.m < /strong></td >
-    <
-    td > Success Factors
-for Women Physician Leaders < /td> <
-    /tr> <
-    tr >
-    <
-    td style = "vertical-align: top;" > < strong > 2: 45 & ndash;
-3 p.m < /strong></td >
-    <
-    td > < b > Break < /b></td >
-    <
-    /tr> <
-    tr >
-    <
-    td style = "vertical-align: top;" > < strong > 3 < /strong><strong style="line-height: 20.8px;">&ndash;</strong > < strong > 4 p.m. < /strong></td >
-    <
-    td > Pathways to Leadership at AGA: Panel Discussion < /td> <
-    /tr> <
-    tr >
-    <
-    td > < strong style = "line-height: 20.8px;" > 4 & ndash;
-5 < /strong><strong style="line-height: 20.8px;">&nbsp;p.m.</strong > < /td> <
-    td > Designing Your Career: Acquiring Competencies and Advisors & nbsp; < /td> <
-/tr> <
-tr >
-    <
-    td style = "vertical-align: top;" > < strong style = "line-height: 20.8px;" > 5 & ndash;
-6 < /strong><strong style="line-height: 20.8px;">&nbsp;p.m.</strong > < /td> <
-    td > Work / Life Balance: Learning How to Say & ldquo;
-No & rdquo; & nbsp; < /td> <
-/tr> <
-tr >
-    <
-    td style = "vertical-align: top;" > < strong style = "line-height: 20.8px;" > 7 & ndash;
-8 < /strong><strong style="line-height: 20.8px;">&nbsp;p.m.</strong > < /td> <
-    td > < strong > Reception < /strong></td >
-    <
-    /tr> <
-    /tbody> <
-    /table>
+// Define our user schema
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
 
-<
-h3 > Saturday, Feb.6, 2016 < /h3>
+// Execute before each user.save() call
+UserSchema.pre('save', function(callback) {
+    var user = this;
 
-<
-table >
-    <
-    tbody >
-    <
-    tr >
-    <
-    td style = "width: 25%;" > < strong > 6: 30 & ndash;
-7: 30 a.m. < /strong></td >
-    <
-    td > < strong > Breakfast < /strong></td >
-    <
-    /tr> <
-    tr >
-    <
-    td style = "vertical-align: top;" > < strong > 7: 30 & ndash;
-8: 30 a.m. < /strong></td >
-    <
-    td > Managing Up and Down in Your Practice or in Academia: Developing Effective Communication Skills & nbsp; < /td> <
-/tr> <
-tr >
-    <
-    td style = "vertical-align: top;" > < strong > 8: 30 & ndash;
-9: 30 a.m. < /strong></td >
-    <
-    td > Problems and Pitfalls
-for Women in Leadership < /td> <
-    /tr> <
-    tr >
-    <
-    td style = "vertical-align: top;" > < strong > 9: 30 & ndash;
-9: 45 a.m. < /strong></td >
-    <
-    td > < strong > Break < /strong></td >
-    <
-    /tr> <
-    tr >
-    <
-    td style = "vertical-align: top;" > < strong > 9: 45 & ndash;
-10: 45 a.m. < /strong></td >
-    <
-    td > Maximizing Committee Effectiveness through Effective Meeting Management and Personality Diversity: PACE Color Palette Exercise < /td> <
-    /tr> <
-    tr >
-    <
-    td > < strong > 10: 45 & ndash;
-11: 45 a.m. < /strong></td >
-    <
-    td > Conflict Resolution: Managing Difficult Conversations < /td> <
-    /tr> <
-    tr >
-    <
-    td > < strong > < span style = "line-height: 20.8px;" > 11: 45 a.m. & ndash;
-12: 45 p.m. < /span></strong > < /td> <
-    td >
-    <
-    p > < strong > Lunch < /strong></p >
+    // Break out if the password hasn't changed
+    if (!user.isModified('password'))
+        return callback();
 
-    <
-    p > Next Steps: Presentations Back Home & nbsp; < /p> <
-/td> <
-/tr> <
-tr >
-    <
-    td > < strong > < span style = "line-height: 20.8px;" > 12: 45 & ndash;
-2: 15 p.m. < /span></strong > < /td> <
-    td > Negotiating Skills Workshop < /td> <
-    /tr> <
-    tr >
-    <
-    td > < strong > < span style = "line-height: 20.8px;" > 2: 15 & ndash;
-2: 30 p.m < /span></strong > < /td> <
-    td > < strong > Break < /strong></td >
-    <
-    /tr> <
-    tr >
-    <
-    td > < strong > < span style = "line-height: 20.8px;" > 2: 30 & ndash;
-3: 30 p.m < /span></strong > < /td> <
-    td > Teamwork: What & rsquo;
-s Your Role ? & mdash;
-The Egg Challenge Team Competition < /td> <
-    /tr> <
-    tr >
-    <
-    td > < strong > 3: 30 p.m. < /strong></td >
-    <
-    td > < strong > < span style = "line-height: 20.8px;" > Wrap Up and Send Off < /span></strong > < /td> <
-    /tr> <
-    /tbody> <
-    /table>--> <
-    p > & nbsp; < /p>
+    // Password changed so we need to hash it
+    bcrypt.genSalt(5, function(err, salt) {
+        if (err)
+            return callback(err);
+
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err)
+                return callback(err);
+            user.password = hash;
+            callback();
+        });
+    });
+});
+
+//Verify password to authenticate calls to API
+UserSchema.methods.verifyPassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        if (err)
+            return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+
+// Export the Mongoose model
+module.exports = mongoose.model('User', UserSchema);
---
+++ sample.js
@@ -0,0 +1,81 @@
+// Load required packages
+var Course = require('../models/course');
+
+// Create endpoint /api/courses for POSTS
+exports.postCourses = function( req, res ){
+    // Create a new instance of the course model
+    var course = new Course();
+
+    // Set the course properties that came from the POST data
+    course.progress = req.body.progress;
+    course.imageUrl = req.body.imageUrl;
+    course.experienceLevel = req.body.experienceLevel;
+    course.language = req.body.language;
+    course.description = req.body.description;
+    course.title = req.body.title;
+    course.userId = req.user._id;
+
+    // Save the course and check for errors
+    course.save(function( err ){
+        if (err)
+            res.send(err);
+
+        res.json({
+            message: 'New course created!',
+            data:    course
+        });
+    });
+};
+
+// Create endpoint /api/courses for GET all courses
+exports.getCourses = function( req, res ){
+    // Use the Course model to find all courses
+    Course.find({ userId: req.user._id }, function( err, courses ){
+        if (err)
+            res.send(err);
+
+        res.json(courses);
+    });
+};
+
+
+// Create endpoint /api/courses/:course_id for GET
+exports.getCourse = function( req, res ){
+    // Use the Course model to find a specific course
+    Course.find({
+          userId: req.user._id,
+          _id:    req.params.course_id
+      },
+      function( err ){
+          if (err)
+              res.send(err);
+
+          res.json(course);
+      });
+};
+
+// Create endpoint /api/courses/:courses_id for PUT
+exports.putCourse = function( req, res ){
+    // Use the Course model to find a specific course
+    Course.update({ userId: req.user._id, _id:    req.params.course_id }, { quantity: req.body.quantity },
+      function( err, num, raw ){
+        if (err)
+            res.send(err);
+
+        res.json({ message: num + ' updated' });
+    });
+};
+
+
+// Create endpoint /api/courses/:course_id for DELETE
+exports.deleteCourse = function( req, res ){
+    // Use the Course model to find a specific course and remove it
+    Course.remove({ userId: req.user._id, _id: req.params.course_id },
+      function( err ){
+        if (err)
+            res.send(err);
+
+        res.json({ message: "Course has been deleted!" });
+    });
+};
+
