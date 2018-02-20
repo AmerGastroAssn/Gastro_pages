36f94e6e4ce8f584f40336aab3d2c965bda2f879
cbstodd <cbstodd@gmail.com>
2018-02-20 11:54:01 -0500

    Added mem recognition prize pages

---
+++ index.ts
@@ -0,0 +1,6 @@
+https://www.youtube.com/embed/ItlE8aR9QlQ
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ItlE8aR9QlQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+
+<iframe src="https://www.youtube.com/embed/ItlE8aR9QlQ?wmode=opaque" gesture="media" allow="encrypted-media" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
